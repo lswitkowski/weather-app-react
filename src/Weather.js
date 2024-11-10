b698db929f9c4dfb8b73d7840a6fbffa1fc24f06
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-10 12:31:36 -0800

    Start building API call

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,12 @@
 import "./Weather.css";
 
 export default function Weather(){
+
+    const apiKey = "65ae2e8ao4f01409ca53644a9atfcbed";
+let city = "London";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+
     return (
       <div className="Weather p-5 m-4">
         <form className="m-1">
@@ -11,10 +17,10 @@ export default function Weather(){
           />
           <input className="form-button" type="submit" value="Search" />
         </form>
-        <h1 class="mt-5 mb-5">New York</h1>
-       
-        <div className="row mt-4 ">
-          <div className="col-3">
+        <h1 class="mt-5 mb-">New York</h1>
+
+        <div className="row mt-4">
+          <div className="col-4">
             <ul>
               <li>Wednesday 7:00</li>
               <li>Mostly Cloudy</li>
@@ -30,7 +36,7 @@ export default function Weather(){
             <span className="temperature">6</span>
             <span className="unit">℃</span>
           </div>
-          <div className="col-5">
+          <div className="col-4">
             <ul>
               <li>Precipitation: 15%</li>
               <li>Humidity: 72%</li>
