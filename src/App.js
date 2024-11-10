3279d45370e374c74621d8515e702ff341479384
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-09 16:58:41 -0800

    Basic Structure/ footer
    Removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+   <h1>Weather App</h1>
+      <footer>
+        This website was coded by{" "}
+        <a
+          href="https://github.com/lswitkowski/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Lisa W
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/lswitkowski?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted{" "}
+        <a
+          href="https://www.netlify.com/"
+          target="_blank"
+          rel="noreferrer"
+          title="netlify"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
