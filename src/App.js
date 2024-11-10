f2bbf07a572d9e69ee38a74fbe299701947fb756
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-11-10 11:31:01 -0800

    Remove unused files, css

--- src/App.js
+++ src/App.js
@@ -5,36 +5,37 @@ import Weather from "./Weather.js";
 
 export default function App() {
   return (
-    <div className="App"><div className="container">
-   <h1>Weather App</h1>
-   <Weather/>
-      <footer>
-        This website was coded by{" "}
-        <a
-          href="https://github.com/lswitkowski/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Lisa W
-        </a>
-        , open-sourced on{" "}
-        <a
-          href="https://github.com/lswitkowski?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        , and hosted on{" "}
-        <a
-          href="https://www.netlify.com/"
-          target="_blank"
-          rel="noreferrer"
-          title="netlify"
-        >
-          Netlify
-        </a>
-      </footer>
-    </div></div>
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This website was coded by{" "}
+          <a
+            href="https://github.com/lswitkowski/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lisa W
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/lswitkowski?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted on{" "}
+          <a
+            href="https://www.netlify.com/"
+            target="_blank"
+            rel="noreferrer"
+            title="netlify"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,25 +2,35 @@ import "./Weather.css";
 
 export default function Weather(){
     return (
-      <div className="Weather">
-        <form>
-            <input className="form-input" type="search" placeholder="Enter a city"/>
-        <input className="form-button" type="submit" value="Search"/>
+      <div className="Weather p-5 m-4">
+        <form className="m-1">
+          <input
+            className="form-input"
+            type="search"
+            placeholder="Enter a city"
+          />
+          <input className="form-button" type="submit" value="Search" />
         </form>
-        <h1>New York</h1>
-        <ul>
-          <li>Wednesday 7:00</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
+        <h1 class="mt-5 mb-5">New York</h1>
+       
+        <div className="row mt-4 ">
+          <div className="col-3">
+            <ul>
+              <li>Wednesday 7:00</li>
+              <li>Mostly Cloudy</li>
+            </ul>
+          </div>
+
+          <div className="col-4">
             <img
+              className="weather-icon"
               src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
               alt="sunny logo"
             />
-            6℃
+            <span className="temperature">6</span>
+            <span className="unit">℃</span>
           </div>
-          <div className="col-6">
+          <div className="col-5">
             <ul>
               <li>Precipitation: 15%</li>
               <li>Humidity: 72%</li>
